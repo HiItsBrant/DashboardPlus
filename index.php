64332f51dfceb649dca1741428516477a81af8d4
Brant <brant.marshall@gmail.com>
2016-06-03 03:17:39 -0400

    Added former DBP Template
    Template Added

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>DashboardPlus | Your Twitch Dashboard, Reloaded.</title>
+<link rel="stylesheet" type: "text/css" href="styles/global.css">
+</head>
+<body>
+  <div id="header">
+    <div class="logo"><a href="#">Dashboard<span>Plus</span></a></div>
+  </div>
+
+  <div id="container">
+    <div class="sidebar">
+        <ul id="nav">
+          <li><a class="active" href="#">Dashboard</a></li>
+          <li><a href="#">Giveaway</a></li>
+          <li><a href="#">Show Notes</a></li>
+          <li><a href="#">Chat Logging</a></li>
+          <li><a href="#">Moderation Tools</a></li>
+          <li><a href="#">Log Out</a></li>
+        </ul>
+    </div>
+
+
+    <div class="content-box">
+        <h1>Dashboard</h1>
+        <p>Welcome, $user.</p>
+
+        <div id="box">
+          <div class="box-top">Panel Name</div>
+          <div class="box-pane">
+            Insert Page Content
+          </div>
+        </div>
+
+    </div>
+  </div>
+</body>
+</html>
